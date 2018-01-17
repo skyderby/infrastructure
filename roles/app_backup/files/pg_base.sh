0bef1668dfc241ddc374a640fa30fb60d3ed3241
Aleksandr Kunin <skyksandr@gmail.com>
2018-01-17 13:08:11 +0300

    abs path for docker-compose

--- roles/app_backup/files/pg_base.sh
+++ roles/app_backup/files/pg_base.sh
@@ -5,7 +5,7 @@ wal_files=$(docker volume inspect pg_wal_archive --format '{{ .Mountpoint }}')
 cd /opt/app/skyderby
 
 # Perform backup and upload to s3
-docker-compose exec -T db /bin/bash -c "pg_basebackup -F tar -D - -X f --gzip -U rep" | \
+/usr/local/bin/docker-compose exec -T db /bin/bash -c "pg_basebackup -F tar -D - -X f --gzip -U rep" | \
 /usr/local/bin/aws s3 cp - s3://pg-backup.skyderby.ru/pg_backup.tar.gz
 
 # Cleanup old WAL files
